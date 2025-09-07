ac6efbded27b639e7cc24a08785447f8316d8f9b
XINEXPORT <christinehoangt@gmail.com>
2025-09-07 15:18:31 -0500

    clang linter ran for Big_Three

--- Big_Three/Num.cpp
+++ Big_Three/Num.cpp
@@ -1,64 +1,55 @@
 #include "Num.h"
 
-//default constructor
-Num::Num() 
-{
-    valuePtr = new double;
-    *valuePtr = 0.0;
+// default constructor
+Num::Num() {
+  valuePtr = new double;
+  *valuePtr = 0.0;
 }
 
-//overloaded constructor
-//uses shallow copy default
-Num::Num(double initialValue)
-{
-    valuePtr = new double;
-    *valuePtr = initialValue;
-
+// overloaded constructor
+// uses shallow copy default
+Num::Num(double initialValue) {
+  valuePtr = new double;
+  *valuePtr = initialValue;
 }
 
-//copy constructor
-//allows for a deep copy
-Num::Num(const Num &other)
-{
-    // can be written explicitly
-    // valuePtr = new double;
-    // *valuePtr = *other.valuePtr;
+// copy constructor
+// allows for a deep copy
+Num::Num(const Num& other) {
+  // can be written explicitly
+  // valuePtr = new double;
+  // *valuePtr = *other.valuePtr;
 
-    // or using this->
-    this->valuePtr = new double;
-    *this->valuePtr = *other.valuePtr;
+  // or using this->
+  this->valuePtr = new double;
+  *this->valuePtr = *other.valuePtr;
 }
 
-//operator overloader for '=' 
-Num& Num::operator=(const Num &rhs)
-{
-    if(this == &rhs) {
-        return *this;
-    }
+// operator overloader for '='
+Num& Num::operator=(const Num& rhs) {
+  if (this == &rhs) {
+    return *this;
+  }
 
-        delete valuePtr;
+  delete valuePtr;
 
-        valuePtr = new double;
-        *valuePtr = *rhs.valuePtr;
+  valuePtr = new double;
+  *valuePtr = *rhs.valuePtr;
 
-        return *this;
+  return *this;
 }
 
-
-//destructor
-Num::~Num()
-{
-    delete valuePtr;
+// destructor
+Num::~Num() {
+  delete valuePtr;
 }
 
-//getter
-double Num::get()
-{
-    return *valuePtr;
+// getter
+double Num::get() {
+  return *valuePtr;
 }
 
-//setter
-void Num::set(double newValue)
-{
-    *valuePtr = newValue;
+// setter
+void Num::set(double newValue) {
+  *valuePtr = newValue;
 }
