abe5fb54d7f20832de254bb6fbcc8debb79c6c6a
XINEXPORT <christinehoangt@gmail.com>
2025-09-08 18:07:09 -0500

    adding default constructor unit test

---
+++ Big_Three/unit_tests.cpp
@@ -0,0 +1,24 @@
+#include "Num.h"
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+//function declarations
+void test_constructor();
+
+int main() {
+
+    //call the test function
+    test_constructor();
+    cout << "Default Constructor Test PASSED " << endl;
+
+    return 0;
+}
+
+//tests go here
+void test_constructor(){
+    Num my_list;
+
+    assert(my_list.get() == 0);
+}
