e7b765c383c66a434bcd62140b90a1089f87ac40
XINEXPORT <christinehoangt@gmail.com>
2025-09-08 19:30:57 -0500

    adding copy constructor unit test

--- Big_Three/unit_tests.cpp
+++ Big_Three/unit_tests.cpp
@@ -4,18 +4,25 @@
 
 using namespace std;
 
-//function declarations
+//Function Declarations
 void test_constructor();
+void test_copy_constructor();
 
 int main() {
 
-    //call the test function
+    //call the test functions
     test_constructor();
+    test_copy_constructor();
 
     return 0;
 }
 
-//tests go here
+/*
+test_constructor
+Input: None
+Description: Testing the default constructor
+Output: my_list should return true if the value is 0
+*/
 void test_constructor(){
     Num my_list;
 
@@ -23,3 +30,19 @@ void test_constructor(){
 
     cout << "Default Constructor Test PASSED " << endl;
 }
+
+/*
+test_copy_constructor
+Input: None
+Description: Testing the copy constructor
+Output: my_list should return true if the value is 1
+*/
+void test_copy_constructor(){
+    Num my_list(1);
+
+    assert(my_list.get() == 1);
+
+    cout << "Copy Constructor Test PASSED" << endl;
+}
+
+
