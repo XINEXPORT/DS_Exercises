cbba3a806213bf133fcda111ecb8b7b880655e80
XINEXPORT <christinehoangt@gmail.com>
2025-09-13 20:14:38 -0500

    changed *song1 and *song2 to *songs to make a playlist

--- Music_Playlist/playlist.cpp
+++ Music_Playlist/playlist.cpp
@@ -7,9 +7,37 @@
 
 using namespace std;
 
-struct SongListNod{
+struct SongListNode{
   string name;
   SongListNode *next;
 };
 
-int main() { return 0; }
+/*
+* Allocate and intialize a new song list node
+*/
+SongListNode *newSongListNode(string name, SongListNode *next){
+
+  SongListNode *result = new SongListNode;
+
+  result->name = name;
+  result->next = next;
+
+  return result;
+}
+
+SongListNode *newSongListNode(string name) {
+  return newSongListNode(name, nullptr);
+}
+
+int main() {
+  SongListNode *song1 = newSongListNode("more");
+
+  SongListNode *songs = newSongListNode("unforgettable",
+                                        newSongListNode("mona_lisa", song1));
+
+  cout << "First song: " << songs->name << endl;
+  cout << "Second song: " << songs->next->name << endl;
+  cout << "Second song: " << songs->next->next->name << endl;
+
+  return 0;
+}
