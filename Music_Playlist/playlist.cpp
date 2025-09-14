9cb3af655bdd9df933c9cb371ed35dc8e679bedf
XINEXPORT <christinehoangt@gmail.com>
2025-09-13 21:09:26 -0500

    adding recursive recyclelist and loopily recyclelist

--- Music_Playlist/playlist.cpp
+++ Music_Playlist/playlist.cpp
@@ -29,15 +29,48 @@ SongListNode *newSongListNode(string name) {
   return newSongListNode(name, nullptr);
 }
 
+void printListLoopily(SongListNode *first) {
+    while(first != nullptr){
+      cout << first->name << endl;
+      first = first->next;
+    }
+}
+
+//recursive printList
+//the base case is an empty list
+void printListRecurse(SongListNode *first) {
+    if(first != nullptr){
+      cout << first->name << endl;
+      printListRecurse(first->next);
+    }
+}
+
+void recycleListLoopily(SongListNode *first){
+    while(first!= nullptr){
+      SongListNode *temp = first;
+      first = first->next;
+      delete temp;
+    }
+}
+
+void recycleListRecurse(SongListNode *first){
+    if(first != nullptr) {
+        recycleListRecurse(first->next);
+        cerr << "Starting recycle node " << first->name << " at " << first << endl;
+        delete first;
+    }
+}
+
 int main() {
   SongListNode *song1 = newSongListNode("more");
 
   SongListNode *songs = newSongListNode("unforgettable",
                                         newSongListNode("mona_lisa", song1));
 
-  cout << "First song: " << songs->name << endl;
-  cout << "Second song: " << songs->next->name << endl;
-  cout << "Second song: " << songs->next->next->name << endl;
+//   printListLoopily(songs);
+//   printListRecurse(songs);
+//   recycleListRecurse(songs);
+  recycleListLoopily(songs);
 
   return 0;
 }
