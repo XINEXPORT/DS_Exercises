a8ce2d4e63cc35a8a7d0736844d7c0a97366e4b3
XINEXPORT <christinehoangt@gmail.com>
2025-09-13 19:43:43 -0500

    adding music_playlist directory and playlist.cppp

--- ArrayList/testIntArrayList.cpp
+++ ArrayList/testIntArrayList.cpp
@@ -9,7 +9,8 @@ int main() {
 
     cout << "initial size: " << al1.size() << endl;
     al1.addAtBack(-12);
-    cout << "size should be 1: " << al1.size() << ";" << " element should be -12: " << al1.elementAt(0) <<endl;
+    cout << "size should be 1: " << al1.size() << ";"
+    << " element should be -12: " << al1.elementAt(0) <<endl;
 
     return 0;
 }
---
+++ Music_Playlist/playlist.cpp
@@ -0,0 +1,15 @@
+/*
+* A music playlist
+* Example of a basic linked structure and manipulation without classes.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+struct SongListNod{
+  string name;
+  SongListNode *next;
+};
+
+int main() { return 0; }
