5e4ad0e805d97f004b95a1f6da00f865d3726c15
XINEXPORT <christinehoangt@gmail.com>
2025-09-22 22:29:32 -0500

    format ran

--- Circular_Array/array_On.cpp
+++ Circular_Array/array_On.cpp
@@ -1,24 +1,24 @@
-//array_On.cpp
-//O(n) runtime
+// array_On.cpp
+// O(n) runtime
 
 #include <bits/stdc++.h>
 using namespace std;
 
-void print (char arr[], int numItems, int idx){
+void print(char arr[], int numItems, int idx) {
     char copy_arr[(2 * numItems)];
 
-    for(int i  = 0; i < numItems; i++){
+    for (int i = 0; i < numItems; i++) {
         copy_arr[i] = copy_arr[numItems + i] = arr[i];
     }
 
-    for(int i = idx; i < numItems + idx; i++){
+    for (int i = idx; i < numItems + idx; i++) {
         cout << copy_arr[i] << " ";
     }
 }
 
-int main(){
+int main() {
     char arr[] = {'C', 'H', 'R', 'I', 'S'};
     int n = sizeof(arr) / sizeof(arr[0]);
-    print (arr, n, 4);
+    print(arr, n, 4);
     return 0;
 }
--- Circular_Array/array_o1.cpp
+++ Circular_Array/array_o1.cpp
@@ -1,20 +1,18 @@
-//array_o1.cpp
-//O(1) runtime
+// array_o1.cpp
+// O(1) runtime
 
 #include <iostream>
 using namespace std;
 
 void print(char arr[], int numItems, int idx) {
-
     for (int i = idx; i < numItems + idx; i++) {
         cout << arr[(i % numItems)] << " ";
-
     }
 }
 
-int main(){
+int main() {
     char arr[] = {'C', 'H', 'R', 'I', 'S'};
     int n = sizeof(arr) / sizeof(arr[0]);
-    print (arr, n, 2);
+    print(arr, n, 2);
     return 0;
 }
