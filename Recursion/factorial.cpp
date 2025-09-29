97ed0e48aeff5704d0c52b002ee6397d55899f00
XINEXPORT <christinehoangt@gmail.com>
2025-09-28 19:53:39 -0500

    adding factorial and power example

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+long factorial(int n) {
+    if(n == 0){
+        return 1;
+    } else {
+        cout << n * factorial(n - 1) << endl;
+        ;
+        return n * factorial(n - 1);
+    }
+}
+
+int main(){
+    long result = factorial(4);
+    cout << "Factorial: " << result << endl;
+    return 0;
+}
---
+++ Recursion/power.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+double power(double base, int expo) {
+    if(expo == 0) {
+        return 1;
+    } else {
+        cout << base * power(base, expo - 1) << endl;
+        return base * power(base, expo - 1);
+    }
+}
+
+int main(){
+    double result = power(3, 3);
+    cout << "Power: " << result << endl;
+    return 0;
+}
