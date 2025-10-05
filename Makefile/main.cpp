210e8bf8ca4fc4dc8ed18849f531e5c798063f06
XINEXPORT <christinehoangt@gmail.com>
2025-10-05 18:28:12 -0500

    adding main file

---
+++ Makefile/main.cpp
@@ -0,0 +1,27 @@
+//main.cpp
+
+#include <iostream>
+#include "Point.h"
+#include "Rectangle.h"
+
+using namespace std;
+
+void print_rectangles(Rectangle rectangles[], int num_rectangles);
+
+int main(){
+    //make an array of reactangles to play with
+
+                              // rect 1
+    Rectangle rectangles[3] = {Rectangle(Point(3, 4), Point(8, 15)),
+                               // rect 2
+                               Rectangle(Point(1, 20), Point(17, 22)),
+                               // rect 3
+                               Rectangle(Point(30, 1), Point(40, 8))};
+
+    cout << "Here are the rectangles, print : " << endl;
+
+    print_rectangles(rectangles, 3);
+    cout << endl;
+
+    return 0;
+}
