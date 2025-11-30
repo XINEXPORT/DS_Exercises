c9972356731299b7f066137d1f2c32bbfd6eaea2
XINEXPORT <christinehoangt@gmail.com>
2025-11-29 23:50:19 -0600

    adding example of insertSort

--- Sorting/insertSort.cpp
+++ Sorting/insertSort.cpp
@@ -1,4 +1,6 @@
-//insertSort.cpp
+// insertSort.cpp
+
+#include <iostream>
 
 // Not in-place: start with an empty result list, insert each item from the
 // original list in order (keep the result list sorted).
@@ -7,6 +9,39 @@
 // Divide array into sorted and unsorted parts (first element is already
 // sorted). For each unsorted element, slide it into sorted position.
 
-int main(){
+void shift(int values[], int i) {
+    int key = values[i];
+    int j = i - 1;
+
+    while (j >= 0 and values[j] > key) {
+        values[j + 1] = values[j];
+        j--;
+    }
+    values[j + 1] = key;
+}
+
+void insertSort(int values[], int size) {
+    for (int i = 1; i < size; i++) {
+        shift(values, i);
+    }
+}
+
+// recursive
+void insertRecurse(int values[], int from, int to) {
+    if (from > to) return;
+    shift(values, from);
+    insertRecurse(values, from + 1, to);
+}
+
+int main() {
+    int values[] = {55, 54, 1, 7, 13};
+    int size = 5;
+
+    insertRecurse(values, 1, size - 1);
+
+    for (int i = 0; i < size; i++) {
+        std::cout << values[i] << " ";
+    }
+
     return 0;
 }
