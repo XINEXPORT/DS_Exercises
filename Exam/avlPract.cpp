c31677017cd20fa2efbb8786c0395fdb33381257
XINEXPORT <christinehoangt@gmail.com>
2025-12-02 19:49:18 -0600

    adding big o comments in sorting algos and pseudocode for avl in Exam

---
+++ Exam/avlPract.cpp
@@ -0,0 +1,11 @@
+//avl
+
+//start with a BST
+//look for an imbalance
+//count the tree's height
+
+//look for the heights of each branch
+//look for the node where the imbalance occurs
+
+//ll rotation
+//rr rotation
